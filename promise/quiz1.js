df97e4836c9019cb69451ccd811c395af6f217c9
ryanj8512 <ryanjan8512@gmail.com>
2021-08-14 21:51:52 +0800

    修正 quiz1.js 執行緒解釋

--- promise/quiz1.js
+++ promise/quiz1.js
@@ -15,11 +15,10 @@ function syncF() {
 
   /*
   執行順序: 4 -> 1 -> 3 -> 5 -> 2
-  1. function syncF() 是一個 callback function
-  2. 先回傳 console.log(4)
-  3. syncF() 呼叫 function syncF()
-  4. 按順序先執行 console.log(1)
-  5. 再執行 console.log(3)，因為 setTimeout是marco
-  6. stock 執行完最後 console.log(5)
-  7. 最後執行 setTimeour (2)
+  1. 依照由上至下執行順序，先呼叫 (4)
+  2. 處理 syncf() 裡面函式
+  3. 在函式裡先執行 (1)
+  4. 遇到 setTimeout() 會先移到 queue 裡面，等待 stack 執行空了在處理
+  5. 接下來處理 (3)，(5)
+  6. 等待 stack 執行任務結束後，even loop 將 queue 裡面的 setTimeout() 移到 stack 執行 (2)
   */
