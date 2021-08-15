a250aa49953236b8e7f6fd769c908c144296d1e0
ryanj8512 <ryanjan8512@gmail.com>
2021-08-16 01:28:06 +0800

    建立 callback 版本爬蟲

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -14,17 +14,22 @@ _=1629032898359
 // 通常會用 const，引用什麼套件?
 const axios = require("axios");
 const moment = require("moment");
+const fs = require("fs");
 
-// axios 寫法，因為是 promise base，後面要 .then
-axios
-  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-    params: {
-      response: "json",
-      date: moment().format("YYYYMMDD"), // 可以自動抓日期
-      stockNo: "2330",
-    },
-  })
-  // response 一個請求會返回的回應
-  .then((response) => {
-    console.log(response.data); // 取得 data 資料
-  });
+// utf8 為人類可以看得懂的語言
+// 下面為 callback 非同步，避免stockNo因為非同步最後才讀取檔案，而axios以下函式放進去執行
+fs.readFile("stock.txt", "utf8", (err, stockCode) => {
+  // axios 寫法，因為是 promise base，後面要 .then
+  axios
+    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"), // 可以自動抓日期
+        stockNo: stockCode, // 再不修改程式的情況下可以自由讀取所需要的資料
+      },
+    })
+    // response 一個請求會返回的回應
+    .then((response) => {
+      console.log(response.data.title); // 取得 data 資料
+    });
+});
