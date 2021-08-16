beed84ffb53bbc04dde2564e748140805f6ae4bf
ryanj8512 <ryanjan8512@gmail.com>
2021-08-16 15:27:46 +0800

    正確 async 寫法

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -2,37 +2,36 @@ const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 
-async function stockCode() {
-    return await new Promise((resolve, reject) => {
-        fs.readFile("stock.txt", "utf8", (err, stockCode) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(stockCode);
-            }
-        })
-    })
-}
+// 1. 將要做的先取出 new Promise
+// 4. +上await為了避免非同步直接到最後一行 console.log(response.data.title);
+// 6. 使用 async 設定暫停範圍，暫停到非同步結束，回傳resolve後繼續工作
+async function doWork() {
+  let stockCode = await new Promise((resolve, reject) => {
+    fs.readFile("stock.txt", "utf8", (err, stockCode) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stockCode);
+      }
+    });
+  });
 
-async function response(stockCode) {
-    return await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+  // 2. 將要做的先取出 axios.get
+  // 5. +上await為了避免非同步直接到最後一行 console.log(response.data.title);
+  // 7. await 會將 resolve 的結果回傳給 response
+  let response = await axios.get(
+    "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+    {
       params: {
         response: "json",
         date: moment().format("YYYYMMDD"),
         stockNo: stockCode,
       },
-    });
-};
-
-async function doAllWorks() {
-    try {
-        let result1 = await stockCode();
-        console.log(result1);
-        let result2 = await response();
-        console.log(result2);
-    } catch(err) {
-        console.log(err);
     }
+  );
+   // 3. 將要做的先取出，取得資料
+   // 8. 最後執行 response 的輸出
+  console.log(response.data.title);
 }
-doAllWorks();
 
+doWork();
