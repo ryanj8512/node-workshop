ff107159a13e55577cb3c627044dff465c9beb9e
ryanj8512 <ryanjan8512@gmail.com>
2021-08-18 01:52:48 +0800

    callback版本 爬蟲查詢資料

--- crawler/crawler-db.js
+++ crawler/crawler-db.js
@@ -6,24 +6,6 @@ const mysql = require("mysql");
 // 不需要變數接，只需要 require
 require("dotenv").config();
 
-// 資料庫連線設定
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-
-  connection.connect((err) => {
-    if (err) {
-      console.log("資料庫連不上");
-    }
-  });
-
-  // 不關閉連線，會認為程式一直執行
-  connection.end();
-
 async function doWork() {
   let stockCode = await new Promise((resolve, reject) => {
     fs.readFile("stock.txt", "utf8", (err, stockCode) => {
@@ -34,6 +16,40 @@ async function doWork() {
       }
     });
   });
+
+  // 資料庫連線設定
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  
+  connection.connect((err) => {
+    if (err) {
+      console.log("資料庫連不上");
+    }
+  });
+  
+  // mysql語法為 callback 函式
+  // 查詢 stock_ID 是否存在
+  connection.query(
+      // 避免SQL injection，要傳的資料寫成問號，要存的資料用中括號 []，底層會處理好
+    `SELECT * FROM stock WHERE stock_id = ?`,
+    [stockCode],
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+      if (results.length > 0) {
+          console.log("有資料");
+      }
+    }
+  );
+  
+  // 不關閉連線，會認為程式一直執行
+  connection.end();
+  
 
   //   let response = await axios.get(
   //     "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
@@ -48,4 +64,8 @@ async function doWork() {
   //   console.log(response.data.title);
 }
 
-doWork();
+try {
+    doWork();
+} catch(e) {
+    console.error(e);
+}
