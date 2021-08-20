74fed69984a79941eafac130494ad76545e6fe9f
ryanj8512 <ryanjan8512@gmail.com>
2021-08-20 22:18:03 +0800

    使用pool連線、做404、取得特定資料和json格式回傳

--- simpleExpress/server.js
+++ simpleExpress/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connection = require("./utils/db");
 
 // 利用 express 建立一個 express application(應用程式)
 let app = express();
@@ -30,7 +31,33 @@ app.get("/about", function(request, response, next) {
     response.send("About Us");
 })
 
+// 撈出資料庫裡面的股票代碼，可用json格式回傳
+app.get("/stock", async function(req, res, next) {
+    let result = await connection.queryAsync("SELECT * FROM stock");
+    res.json(result);
+})
+
+// 只取得特定資料
+app.get("/stock/:stockCode", async function(req, res, next) {
+    // req.params 固定寫法 .接 : 後面的 stockCode
+    // 網址輸入 http://localhost:3000/stock/2603
+    let result = await connection.queryAsync("SELECT * FROM stock_price WHERE stock_id=?",
+    [req.params.stockCode]);
+    res.json(result);
+})
+
+// 404，上面路由沒有比到就會到這(沒有response就會跑到這)
+app.use((req, res, next) => {
+    res.status(404).json({message: "找不到網頁，掰掰"});
+    // 往下執行
+    next();
+});
+
 // 啟動
-app.listen(3000, function() {
+app.listen(3000, async function() {
+    // 如果使用POOL，就不需要做下面那行連線，需要時POOL會自動建立連線
+    // 在啟動時開啟資料庫連結 (當一個連線時需要這行)
+    // await connection.connectAsync();
     console.log("Web GOGOGO");
 })
+
